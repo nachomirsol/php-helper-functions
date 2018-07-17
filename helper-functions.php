00705868bc64572cc1a154daec785f9f74e94b4f
nachomirsol <nachomirsol@gmail.com>
2018-07-17 17:46:37 +0200

    update comment function order multidimensional array by column

--- helper-functions.php
+++ helper-functions.php
@@ -1,5 +1,5 @@
 <?php
-// function to sort Multidimensional arrays, typical case where you will have to work with database queries result
+// function to sort Multidimensional arrays by column, typical case where you will have to work with database queries result
 /* $result_db = Array ( 
                     [0] => Array ( [num_demanda] => AI00000310 [cod_articulo] => S310127605 [nombre_articulo] => Proteccion triangular izquierd [unidades] => 2.00 [unidades_entrada] => 2.00 [cod_proveedor] => 145 [fecha_demanda] => 2018-07-10 [fecha_entrega] => 2018-07-10 [servido] => 3 [nombre] => SIAT S.p.A. ) 
                     [1] => Array ( [num_demanda] => AI00000310 [cod_articulo] => S3803594 [nombre_articulo] => Reductor eje corto SM [unidades] => 1.00 [unidades_entrada] => 1.00 [cod_proveedor] => 145 [fecha_demanda] => 2018-07-10 [fecha_entrega] => 2018-07-10 [servido] => 3 [nombre] => SIAT S.p.A. ) 
